fa17ed4f81a8ef3da48ebb3813eb1c9c95f27d98
kalchurrrrr <nikolaykalchu@gmail.com>
2023-08-29 18:36:50 +0300

    Ещё одна попытка, надеюсь получится

--- src/main/java/com/streamApiHw/streamApiHw/EmployeeService.java
+++ src/main/java/com/streamApiHw/streamApiHw/EmployeeService.java
@@ -1,55 +1,46 @@
 package com.streamApiHw.streamApiHw;
 
-import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-@Service
 public class EmployeeService {
-    private Map<String, Employee> employeeMap;
-    private final int maxEmployeesCount;
+    private List<Employee> employees;
+    private static final int MAX_EMPLOYEES = 10;
 
     public EmployeeService() {
-        employeeMap = new HashMap<>();
-        maxEmployeesCount = 10;
-    }
-
-    public void addEmployee(Employee employee) {
-        if (employeeMap.size() >= maxEmployeesCount) {
-            throw new EmployeeStorageIsFullException("Достигнуто максимальное количество сотрудников");
-        }
-        String key = generateKey(employee.getFirstName(), employee.getLastName());
-        if (employeeMap.containsKey(key)) {
-            throw new EmployeeAlreadyAddedException("Сотрудник уже добавлен");
-        }
-        employeeMap.put(key, employee);
-    }
-
-    public void removeEmployee(Employee employee) {
-        String key = generateKey(employee.getFirstName(), employee.getLastName());
-        if (employeeMap.remove(key) == null) {
-            throw new EmployeeNotFoundException("Сотрудник не найден");
-        }
-    }
-
-    public Employee findEmployee(String firstName, String lastName) {
-        String key = generateKey(firstName, lastName);
-        return employeeMap.get(key);
+        employees = new ArrayList<>();
     }
 
     public List<Employee> getAllEmployees() {
-        return employeeMap.values().stream().collect(Collectors.toList());
+        return new ArrayList<>(employees);
     }
 
-    private String generateKey(String firstName, String lastName) {
-        return firstName.toLowerCase() + "_" + lastName.toLowerCase();
+    public void addEmployee(Employee employee) {
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException("Сотрудник уже добавлен");
+        }
+
+        if (employees.size() >= MAX_EMPLOYEES) {
+            throw new EmployeeStorageIsFullException("Достигнуто максимальное количество сотрудников");
+        }
+
+        employees.add(employee);
     }
-    public List<Employee> getEmployeesByDepartment(String department) {
-        return employeeMap.values().stream()
-                .filter(employee -> employee.getDepartment().equals(department))
-                .collect(Collectors.toList());
+
+    public void removeEmployee(Employee employee) {
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException("Сотрудник не найден");
+        }
+
+        employees.remove(employee);
+    }
+
+    public Employee findEmployee(String firstName, String lastName) {
+        for (Employee employee : employees) {
+            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
+                return employee;
+            }
+        }
+        return null;
     }
 }
---
+++ src/test/java/com/streamApiHw/streamApiHw/DepartmentServiceTest.java
@@ -0,0 +1,224 @@
+package com.streamApiHw.streamApiHw;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DepartmentServiceTest {
+    private DepartmentService departmentService;
+
+    @Mock
+    private EmployeeService employeeService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        departmentService = new DepartmentService(employeeService);
+    }
+
+    @Test
+    public void testGetEmployeesByDepartment_ReturnsEmployees_WhenDepartmentExists() {
+        List<Employee> expectedEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(expectedEmployees);
+
+        List<Employee> actualEmployees = departmentService.getEmployeesByDepartment("IT");
+
+        assertEquals(expectedEmployees, actualEmployees);
+    }
+
+    @Test
+    public void testGetEmployeesByDepartment_ReturnsEmptyList_WhenDepartmentDoesNotExist() {
+        List<Employee> allEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+
+        List<Employee> actualEmployees = departmentService.getEmployeesByDepartment("Finance");
+
+        assertEquals(Collections.emptyList(), actualEmployees);
+    }
+
+    @Test
+    public void testGetEmployeesByDepartment_ThrowsException_WhenNoDepartmentIdProvided() {
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeesByDepartment(null));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeesByDepartment(""));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeesByDepartment("   "));
+    }
+
+    @Test
+    public void testGetDepartmentSalarySum_ReturnsCorrectSum_WhenDepartmentExists() {
+        List<Employee> employees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(employees);
+
+        double expectedSalarySum = 11000;
+        double actualSalarySum = departmentService.getDepartmentSalarySum("IT");
+
+        assertEquals(expectedSalarySum, actualSalarySum);
+    }
+
+    @Test
+    public void testGetDepartmentSalarySum_ReturnsZero_WhenDepartmentDoesNotExist() {
+        List<Employee> allEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+
+        double actualSalarySum = departmentService.getDepartmentSalarySum("Finance");
+
+        assertEquals(0, actualSalarySum);
+    }
+
+    @Test
+    public void testGetDepartmentSalarySum_ThrowsException_WhenNoDepartmentIdProvided() {
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getDepartmentSalarySum(null));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getDepartmentSalarySum(""));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getDepartmentSalarySum("   "));
+    }
+
+    @Test
+    public void testGetEmployeeWithMaxSalary_ReturnsEmployeeWithMaxSalary_WhenDepartmentExists() {
+        List<Employee> employees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT"),
+                new Employee("Александр", "Пистолетов", 7000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(employees);
+
+        Employee expectedEmployee = new Employee("Александр", "Пистолетов", 7000, "IT");
+        Employee actualEmployee = departmentService.getEmployeeWithMaxSalary("IT");
+
+        assertEquals(expectedEmployee, actualEmployee);
+    }
+
+    @Test
+    public void testGetEmployeeWithMaxSalary_ThrowsException_WhenDepartmentDoesNotExist() {
+        List<Employee> allEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+
+        assertThrows(NoSuchElementException.class,
+                () -> departmentService.getEmployeeWithMaxSalary("Finance"));
+    }
+
+    @Test
+    public void testGetEmployeeWithMaxSalary_ThrowsException_WhenNoEmployeesInDepartment() {
+        List<Employee> allEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+
+        assertThrows(NoSuchElementException.class,
+                () -> departmentService.getEmployeeWithMaxSalary("HR"));
+    }
+
+    @Test
+    public void testGetEmployeeWithMaxSalary_ThrowsException_WhenNoDepartmentIdProvided() {
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeeWithMaxSalary(null));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeeWithMaxSalary(""));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeeWithMaxSalary("   "));
+    }
+
+    @Test
+    public void testGetEmployeeWithMinSalary_ReturnsEmployeeWithMinSalary_WhenDepartmentExists() {
+        List<Employee> employees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT"),
+                new Employee("Александр", "Пистолетов", 4000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(employees);
+
+        Employee expectedEmployee = new Employee("Александр", "Пистолетов", 4000, "IT");
+        Employee actualEmployee = departmentService.getEmployeeWithMinSalary("IT");
+
+        assertEquals(expectedEmployee, actualEmployee);
+    }
+
+    @Test
+    public void testGetEmployeeWithMinSalary_ThrowsException_WhenDepartmentDoesNotExist() {
+        List<Employee> allEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+
+        assertThrows(NoSuchElementException.class,
+                () -> departmentService.getEmployeeWithMinSalary("Finance"));
+    }
+
+    @Test
+    public void testGetEmployeeWithMinSalary_ThrowsException_WhenNoEmployeesInDepartment() {
+        List<Employee> allEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "IT"),
+                new Employee("Ольга", "Романова", 6000, "IT")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+
+        assertThrows(NoSuchElementException.class,
+                () -> departmentService.getEmployeeWithMinSalary("HR"));
+    }
+
+    @Test
+    public void testGetEmployeeWithMinSalary_ThrowsException_WhenNoDepartmentIdProvided() {
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeeWithMinSalary(null));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeeWithMinSalary(""));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> departmentService.getEmployeeWithMinSalary("   "));
+    }
+
+    @Test
+    public void testGetAllEmployeesByDepartment_ReturnsEmployeesGroupedByDepartment() {
+        List<Employee> allEmployees = Arrays.asList(
+                new Employee("Иван", "Иванов", 5000, "HR"),
+                new Employee("Ольга", "Романова", 6000, "IT"),
+                new Employee("Александр", "Пистолетов", 4000, "HR"),
+                new Employee("Аркадий", "Паравозов", 8000, "IT"),
+                new Employee("Владимир", "Централов", 5500, "HR")
+        );
+        when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+
+        departmentService.getAllEmployeesByDepartment();
+
+        verify(employeeService).getAllEmployees();
+    }
+}
